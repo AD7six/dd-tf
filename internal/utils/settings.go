77993478e78f259268bec074e3a0f1b3094c73c9
Andy Dawson <andydawson76@gmail.com>
2025-11-03 20:37:06 +0100

    Small refactor of the settings class
    To be less repetative

--- internal/utils/settings.go
+++ internal/utils/settings.go
@@ -10,34 +10,25 @@ import (
 type Settings struct {
 	APIKey        string
 	AppKey        string
-	APIDomain     string
-	DashboardsDir string
+	APIDomain     string // e.g., "api.datadoghq.com" - depends on which datadog site (https://docs.datadoghq.com/getting_started/site/) the account is in
+	DashboardsDir string // Where dashboard JSON files are stored
 }
 
 func LoadSettings() (*Settings, error) {
 	// Try to load .env file if present
 	_ = godotenv.Load()
 
-	apiKey := os.Getenv("DD_API_KEY")
-	appKey := os.Getenv("DD_APP_KEY")
-	apiDomain := os.Getenv("DD_API_DOMAIN")
-	dashboardsDir := os.Getenv("DASHBOARDS_DIR")
-
-	// Default to US site if not set
-	if apiDomain == "" {
-		apiDomain = "api.datadoghq.com"
+	apiKey, err := getEnvRequired("DD_API_KEY")
+	if err != nil {
+		return nil, err
 	}
-	// Default dashboards output directory
-	if dashboardsDir == "" {
-		dashboardsDir = "data/dashboards"
+	appKey, err := getEnvRequired("DD_APP_KEY")
+	if err != nil {
+		return nil, err
 	}
 
-	if apiKey == "" {
-		return nil, fmt.Errorf("DD_API_KEY environment variable must be set")
-	}
-	if appKey == "" {
-		return nil, fmt.Errorf("DD_APP_KEY environment variable must be set")
-	}
+	apiDomain := getEnv("DD_API_DOMAIN", "api.datadoghq.com")
+	dashboardsDir := getEnv("DASHBOARDS_DIR", "data/dashboards")
 
 	return &Settings{
 		APIKey:        apiKey,
@@ -45,4 +36,20 @@ func LoadSettings() (*Settings, error) {
 		APIDomain:     apiDomain,
 		DashboardsDir: dashboardsDir,
 	}, nil
+}
+
+// get the env variable with a default
+func getEnv(key, def string) string {
+	if v, ok := os.LookupEnv(key); ok && v != "" {
+		return v
+	}
+	return def
+}
+
+// get the env variable or raise an error
+func getEnvRequired(key string) (string, error) {
+	if v, ok := os.LookupEnv(key); ok && v != "" {
+		return v, nil
+	}
+	return "", fmt.Errorf("%s environment variable must be set", key)
 }
