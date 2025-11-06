776f4540194c98a532a39e1f203c0022c3bf6b16
Andy Dawson <andydawson76@gmail.com>
2025-11-06 18:19:31 +0100

    Add a verbose flag,
    Which for now sets DEBUG env variable.
    
    TODO - migrate to slog

--- cmd/dd-tf/main.go
+++ cmd/dd-tf/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"os"
+
 	"github.com/AD7six/dd-tf/internal/commands/dashboards"
 	"github.com/AD7six/dd-tf/internal/commands/version"
 	"github.com/spf13/cobra"
 )
 
+var verbose bool
+
 func main() {
 	root := &cobra.Command{
 		Use:   "dd-tf",
 		Short: "Datadog Terraform management CLI",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if verbose {
+				os.Setenv("DEBUG", "1")
+			}
+		},
 	}
+
+	root.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose/debug output (shows curl commands)")
 
 	root.AddCommand(dashboards.NewDashboardsCmd())
 	root.AddCommand(version.NewVersionCmd())
